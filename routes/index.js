5bdd5dfaae8bcefe61e26d2d2b7294c07f4dd0ce
ignacio.Gaona <ignacio.Gaona@EIITCAD003>
2022-01-03 18:03:28 +0100

    master

--- routes/index.js
+++ routes/index.js
@@ -3,81 +3,50 @@
 
 
 var express = require('express');
+var nodemailer = require('nodemailer');
 var router = express.Router();
 var path = require('path');
 
+
 /* GET home page. */
-router.get('/admin', function(req, res, next) {
+router.get('/admin', function (req, res, next) {
+
+
   res.render('index', { title: 'admin dont try to hack me' });
- // res.render('index');
+  // res.render('index');
 });
-router.get('/', function(req, res, next) {
-  
-  res.sendFile(path.join(__dirname,"htmls","index.html"));
+router.get('/', function (req, res, next) {
+
+  res.sendFile(path.join(__dirname, "htmls", "index.html"));
+});
+
+router.get('/chart', function (req, res, next) {
+
+  res.sendFile(path.join(__dirname, "htmls", "chart.html"));
 });
 
 
 //CSS defined
-/*
-router.get('/assets/css/main.css', function(req, res, next) {
-  
-  console.log(__dirname + '\\htmls\\assets\\css\\main.css');
-  res.sendFile(path.join(__dirname ,'htmls','assets','css','main.css'));
+
+router.get('/assets/:path', function (req, res, next) {
+
+  res.sendFile(path.join(__dirname, 'htmls', 'assets', req.params.path));
+
+});
+router.get('/assets/:path/:path2', function (req, res, next) {
+
+  res.sendFile(path.join(__dirname, 'htmls', 'assets', req.params.path, req.params.path2));
 
 });
 
-router.get('/assets/css/images/bg.jpg', function(req, res, next) {
-  
-  
-  res.sendFile(path.join(__dirname ,'htmls','assets','css','images','bg.jpg'));
+router.get('/assets/:path/:path2/:path3', function (req, res, next) {
+
+  res.sendFile(path.join(__dirname, 'htmls', 'assets', req.params.path, req.params.path2, req.params.path3));
 
 });
+router.get('/assets/:path/:path2/:path3/:path4', function (req, res, next) {
 
-router.get('/assets/css/fontawesome-all.min.css', function(req, res, next) {
-  
-  
-  res.sendFile(path.join(__dirname ,'htmls','assets','css','fontawesome-all.min.css'));
-
-});
-
-router.get('/assets/webfonts/fa-brands-400.woff2', function(req, res, next) {
-  
-  
-  res.sendFile(path.join(__dirname ,'htmls','assets','webfonts','fa-brands-400.woff2'));
-
-});
-router.get('/assets/css/images/overlay.svg', function(req, res, next) {
-  
-  
-  res.sendFile(path.join(__dirname ,'htmls','assets','images','overlay.svg'));
-
-});
-router.get('/assets/webfonts/fa-solid-900.woff2', function(req, res, next) {
-  
-  
-  res.sendFile(path.join(__dirname ,'htmls','assets','webfonts','fa-solid-900.woff2'));
-
-});
-*/
-router.get('/assets/:path', function(req, res, next) {
-
-  res.sendFile(path.join(__dirname ,'htmls','assets',req.params.path));
-
-});
-router.get('/assets/:path/:path2', function(req, res, next) {
-
-  res.sendFile(path.join(__dirname ,'htmls','assets',req.params.path,req.params.path2));
-
-});
-
-router.get('/assets/:path/:path2/:path3', function(req, res, next) {
-
-  res.sendFile(path.join(__dirname ,'htmls','assets',req.params.path,req.params.path2,req.params.path3));
-
-});
-router.get('/assets/:path/:path2/:path3/:path4', function(req, res, next) {
-
-  res.sendFile(path.join(__dirname ,'htmls','assets',req.params.path,req.params.path2,req.params.path3,req.params.path4));
+  res.sendFile(path.join(__dirname, 'htmls', 'assets', req.params.path, req.params.path2, req.params.path3, req.params.path4));
 
 });
 
